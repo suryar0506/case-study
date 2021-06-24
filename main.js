b38ddb2354122655a023978ca9cdf1e2ff785d2c
suryar0506 <72110042+suryar0506@users.noreply.github.com>
2021-06-24 16:40:21 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,39 +1,62 @@
+function preload(){
 
-var objects = [
-"https://images-na.ssl-images-amazon.com/images/I/510qPz1my9L._AC_SX522_.jpg",
-"https://images-na.ssl-images-amazon.com/images/I/711F6-eLS6L._AC_SL1500_.jpg",
-"https://cdn.vox-cdn.com/thumbor/s9HX5CWof2fv80O9fENKe5pXtXg=/0x0:2040x1360/1200x800/filters:focal(877x866:1203x1192)/cdn.vox-cdn.com/uploads/chorus_image/image/66397697/akrales_181019_3014_0770.0.jpg",
-"https://www.petalrepublic.com/wp-content/uploads/2020/09/Ultimate-Guide-to-Lotus-Flowers-Meaning-and-Symbolism-scaled.jpg",
-"https://landofbelle.com/wp-content/uploads/2018/06/Land-of-Belle_Website-Product-60.jpg",
+}
+
+function setup(){
+canvas = createCanvas(300, 300);
+canvas.position(700, -10);
+video = createCapture(VIDEO);
+video.hide();
+classifier = ml5.imageClassifier('MobileNet', modelLoaded);
+}
+
+function modelLoaded(){
+    console.log("Model is loaded");
+}
+
+function draw(){
+image(video, 0, 0, 300, 300);
+classifier.classify(video, getResult);
+}
+
+function getResult(error, result){
+if(error){
+    console.log(error);
+} else {
+    console.log(result);
+    document.getElementById("realtime_mobilenet_result").innerHTML = result[0].label;
+}
+}
+
+var images = [
+ "Fork.jpg", "Paintbrush.jpg", "Pen.jpg", "Scissors.jpg","Ball.jpg",
 ];
 
-var azure_names = [
-"Sandglass",
-"Weapon",
-"Screenshot",
-"Food",
-"Vase",
+var mobilenet_results = [
+    "swab/swop/mop","ballpoint/ballpoint pen/biro","oscilloscope","swab/swop/mop","Balloon",
 ];
 
-var mobilenet_name = [
-"Hourglass",
-"Microphone",
-"Cellphone",
-"Ski Mask",
-"Kimono",
+var googleLens_results = [
+    "Fork", "Brush", "Nail", "Scissors","Green Ball",
 ];
 
 var i = 0;
 
-function changeImage(){
-    document.getElementById("object_img").src = objects[i];
-    document.getElementById("azure_result_display").innerHTML = azure_names[i];
-    document.getElementById("mobilenet_result_display").innerHTML = mobilenet_name[i];
+function imageSlideshow(){
+    document.getElementById("imageDisplay").src = images[i];
+    document.getElementById("mobilenet_result").innerHTML = mobilenet_results[i];
+    document.getElementById("googleLens_results").innerHTML = googleLens_results[i];
     i++;
     if(i == 5){
         i = 0;
     }
 }
- function final_results(){
-     document.getElementById("final_conclusion_display").innerHTML = "Based on the results of this case study between Microsoft Azure and Mobilenet, I proclaim Azure to be equal to its counterpart due to its score of 2/5 as opposed to Mobilenet's score of 2/5 objects correct."
- }
+
+function final_conclusion(){
+    document.getElementById("final_conclusion_display").innerHTML = "The results that I've gathered, state that Google lens is more accurate than Mobilenet.";
+}
+
+function empty_of_text(){
+    document.getElementById("final_conclusion_display").innerHTML = "";
+    console.log("I'm supposed to work...");
+}
